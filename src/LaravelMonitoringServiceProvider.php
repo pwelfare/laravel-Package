71c6dfceaca71a11a47ae5436af9cfe1a2f3be95
kiariep <kiariepeter13@gmail.com>
2024-10-29 12:50:28 +0300

    initial Commit

---
+++ src/LaravelMonitoringServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+namespace Stanbic\LaravelMonitoring;
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\ServiceProvider;
+use Prometheus\CollectorRegistry;
+use Prometheus\Storage\redis;
+
+class LaravelMonitoringServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register(): void
+
+    {
+    
+    $this->app->singleton(CollectorRegistry::class, function () {
+    
+    Redis::setDefaultOptions(
+        Arr::only( config('database.redis.default' ), [ 'host', 'password', 'username' ] )
+    );
+    return CollectorRegistry::getDefault();
+    } );
+    
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot ()
+    {
+        //
+    }
+}
